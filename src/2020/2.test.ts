6eaa872298f5638183585ac63b60b2efd8f0ab56
Indimeco <18373729+Indimeco@users.noreply.github.com>
2021-11-28 06:50:19 +1100

    setup aoc for 2021

--- src/2020/2.test.ts
+++ src/2020/2.test.ts
@@ -1,5 +1,101 @@
-import {solution} from './2';
+import {
+  countOldTobogganPasswords,
+  countValidCorporatePasswords,
+  isValidCorporateTobogganEntry,
+  isValidOldTobogganEntry,
+  parseEntry,
+  parseInputToEntries,
+} from "./2";
+import { input } from "./2.input";
 
-it('work', () => {
-  expect(solution()).toEqual(true);
+it("should convert an entry to datum", () => {
+  expect(parseEntry("6-7 n: jbncncnn")).toEqual({
+    min: 6,
+    max: 7,
+    target: "n",
+    data: "jbncncnn",
+  });
+});
+
+it("should convert input to entries", () => {
+  expect(
+    parseInputToEntries(`6-7 s: xsvmsds
+    6-7 n: jbncncnn`)
+  ).toEqual([
+    {
+      min: 6,
+      max: 7,
+      target: "s",
+      data: "xsvmsds",
+    },
+    {
+      min: 6,
+      max: 7,
+      target: "n",
+      data: "jbncncnn",
+    },
+  ]);
+});
+
+it("should pass a valid entry", () => {
+  expect(
+    isValidOldTobogganEntry({
+      min: 6,
+      max: 7,
+      target: "s",
+      data: "xssssvmsds",
+    })
+  ).toEqual(true);
+});
+
+it("should fail an invalid entry", () => {
+  expect(
+    isValidOldTobogganEntry({
+      min: 6,
+      max: 7,
+      target: "s",
+      data: "xsvmsds",
+    })
+  ).toEqual(false);
+});
+
+it("should get the solution for old toboggan", () => {
+  expect(countOldTobogganPasswords(input)).toEqual(393);
+});
+
+it("should pass a valid corporate entry", () => {
+  expect(
+    isValidCorporateTobogganEntry({
+      min: 1,
+      max: 3,
+      data: "abcde",
+      target: "a",
+    })
+  ).toEqual(true);
+});
+
+it("should fail an invalid corporate entry", () => {
+  expect(
+    isValidCorporateTobogganEntry({
+      min: 1,
+      max: 3,
+      data: "cdefg",
+      target: "b",
+    })
+  ).toEqual(false);
+});
+
+it("should fail an invalid corporate entry because both conditions are met", () => {
+  expect(
+    isValidCorporateTobogganEntry({
+      min: 2,
+      max: 9,
+      data: "ccccccccc",
+      target: "c",
+    })
+  ).toEqual(false);
+});
+
+it("should get the solution for corporate toboggan", () => {
+  expect(countValidCorporatePasswords(input)).toEqual(690);
 });
--- src/2020/2.ts
+++ src/2020/2.ts
@@ -1,1 +1,50 @@
-export const solution = () => true;
+import { flow, pipe } from "fp-ts/lib/function";
+import { filter } from "fp-ts/lib/ReadonlyArray";
+import { map, ReadonlyNonEmptyArray } from "fp-ts/lib/ReadonlyNonEmptyArray";
+import { split } from "fp-ts/lib/string";
+
+export const parseInputToEntries = (
+  input: string
+): ReadonlyNonEmptyArray<Entry> => pipe(input, split("\n"), map(parseEntry));
+
+type Entry = { min: number; max: number; target: string; data: string };
+export const parseEntry = (entry: string): Entry => {
+  const [_, min, max, target, data] =
+    entry.match(/(\d+)\-(\d+)\s(\w)\:\s(.*)/) || [];
+  return {
+    min: Number(min),
+    max: Number(max),
+    target,
+    data,
+  };
+};
+
+const countTrue = flow(
+  filter((a) => a === true),
+  (arr) => arr.length
+);
+
+export const isValidOldTobogganEntry = (entry: Entry): boolean =>
+  pipe(
+    [...entry.data.matchAll(new RegExp(entry.target, "g"))].length,
+    (matches) => matches >= entry.min && matches <= entry.max
+  );
+
+export const countOldTobogganPasswords = flow(
+  parseInputToEntries,
+  map(isValidOldTobogganEntry),
+  countTrue
+);
+
+export const isValidCorporateTobogganEntry = (entry: Entry): boolean =>
+  pipe([...entry.data], (arr) =>
+    arr[entry.min - 1] === entry.target
+      ? arr[entry.max - 1] !== entry.target
+      : arr[entry.max - 1] === entry.target
+  );
+
+export const countValidCorporatePasswords = flow(
+  parseInputToEntries,
+  map(isValidCorporateTobogganEntry),
+  countTrue
+);
