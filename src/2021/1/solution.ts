c140a5a79c8ad440601dc5f64657449cee10f7c4
Indimeco <18373729+Indimeco@users.noreply.github.com>
2021-12-03 08:37:35 +1100

    fp-ts refactor

--- src/2021/1/solution.ts
+++ src/2021/1/solution.ts
@@ -1,20 +1,24 @@
-export const solution = (input: number[]): number => {
-  const a = input.reduce(
-    (prev, current) => {
-      return {
-        num: current,
-        count: current > prev.num ? prev.count + 1 : prev.count,
-      };
-    },
-    { num: input[0], count: 0 }
-  );
-  return a.count;
-};
+import { reduce, mapWithIndex, filter } from "fp-ts/Array";
+import { flow } from "fp-ts/lib/function";
 
-export const sum3 = (input: number[]): number[] =>
-  input
-    .map((v, index) => {
+type Solution = (input: number[]) => number;
+export const solution: Solution = flow(
+  (input) =>
+    reduce<number, accumulator>({ num: input[0], count: 0 }, reducer)(input),
+  (reduced) => reduced.count
+);
+
+type accumulator = { num: number; count: number };
+const reducer = (prev: accumulator, current: number) => ({
+  num: current,
+  count: current > prev.num ? prev.count + 1 : prev.count,
+});
+
+export const sum3: (input: number[]) => number[] = flow(
+  (input) =>
+    mapWithIndex((index: number) => {
       if (input[index - 2] !== undefined)
         return input[index] + input[index - 1] + input[index - 2];
-    })
-    .filter((a): a is number => typeof a === "number");
+    })(input),
+  filter((a: number | undefined): a is number => typeof a === "number")
+);
