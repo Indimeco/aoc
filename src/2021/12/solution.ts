16305d89b9d9bd7711033cfb431bce8086cb52fa
Indimeco <18373729+Indimeco@users.noreply.github.com>
2021-12-12 19:44:30 +1100

    refactor 12

--- src/2021/12/solution.ts
+++ src/2021/12/solution.ts
@@ -1,98 +1,86 @@
-import { strong } from "fp-ts";
+import { flow } from "fp-ts/lib/function";
+import { map, reduce, filter, some } from "fp-ts/lib/Array";
 
-export const solution = () => true;
-
-export const parseCaveMap = (
-  input: [string, string][]
-): Record<string, Array<string>> =>
-  Object.fromEntries(
-    Object.entries(
-      input.reduce(
-        (acc: Record<string, Set<string>>, [start, end]: [string, string]) => {
-          if (!acc[start]) {
-            acc[start] = new Set();
-          }
-          if (!acc[end]) {
-            acc[end] = new Set();
-          }
-          acc[start].add(end);
-          acc[end].add(start);
-          return acc;
-        },
-        {}
-      )
-    ).map(([key, set]) => [key, Array.from(set)])
-  );
+export const parseCaveMap = flow(
+  (input: [string, string][]) =>
+    input.reduce(
+      (acc: Record<string, Set<string>>, [start, end]: [string, string]) => {
+        if (!acc[start]) {
+          acc[start] = new Set();
+        }
+        if (!acc[end]) {
+          acc[end] = new Set();
+        }
+        acc[start].add(end);
+        acc[end].add(start);
+        return acc;
+      },
+      {}
+    ),
+  Object.entries,
+  map(([key, set]) => [key, Array.from(set)]),
+  Object.fromEntries
+);
 
 const isSmallCave = (cave: string) =>
   cave.toLowerCase() === cave && cave !== "start";
 
-export const parseInput = (input: string): [string, string][] =>
-  input.split("\n").map((x) => x.split("-").map((x) => x.trim()));
+export const parseInput = flow(
+  (input: string) => input.split("\n"),
+  map((toFrom: string) => toFrom.trim().split("-") as [string, string])
+);
 
 export const pather = (
   currentPath: string[],
   caveMap: Record<string, Array<string>>,
-  allPaths: string[][]
+  allPaths: string[][],
+  sillyPart2Rule: boolean = false
 ): string[][] => {
   const currentCave = currentPath[currentPath.length - 1];
   if (currentCave === "end") return [...allPaths, currentPath];
-  const pathableCaves = caveMap[currentCave].filter(
-    (destination) =>
-      destination !== "start" &&
-      !(isSmallCave(destination) && currentPath.includes(destination))
-  );
+  const pathableCaves = caveMap[currentCave].filter((destination) => {
+    if (destination === "start") return false;
+    const smallCaveAlreadyVisited =
+      isSmallCave(destination) && currentPath.includes(destination);
+    if (sillyPart2Rule) {
+      return hasVisitedSmallCaveTwice(currentPath)
+        ? !smallCaveAlreadyVisited
+        : true;
+    }
+    return !smallCaveAlreadyVisited;
+  });
   if (pathableCaves.length < 1) {
     return allPaths;
   }
   return pathableCaves.flatMap((pathable) =>
-    pather([...currentPath, pathable], caveMap, allPaths)
+    pather([...currentPath, pathable], caveMap, allPaths, sillyPart2Rule)
   );
 };
 
-export const numPathsCaves = (input: string) => {
-  const map = parseCaveMap(parseInput(input));
-  const paths = pather(["start"], map, []);
-  return paths.length;
-};
+export const numPathsCaves = flow(
+  parseInput,
+  parseCaveMap,
+  (caveMap) => pather(["start"], caveMap, []),
+  (allPaths) => allPaths.length
+);
 
-const hasVisitedSmallCaveTwice = (path: string[]) =>
-  Object.values(
-    path
-      .map((a) => (isSmallCave(a) ? a : null))
-      .reduce((acc, cur) => {
-        if (cur == null) return acc;
-        acc[cur] = (acc[cur] || 0) + 1;
-        return acc;
-      }, {})
-  ).some((val) => val > 1);
+const hasVisitedSmallCaveTwice = flow(
+  map((x: string) => (isSmallCave(x) ? x : null)),
+  filter((x): x is string => x !== null),
+  reduce<string | number, Record<string, number>>(
+    {},
+    (accumulator, current) => ({
+      ...accumulator,
+      [current]: (accumulator[current] ?? 0) + 1,
+    })
+  ),
+  Object.values,
+  some((count) => count > 1)
+);
 
-export const pather2 = (
-  currentPath: string[],
-  caveMap: Record<string, Array<string>>,
-  allPaths: string[][]
-): string[][] => {
-  const currentCave = currentPath[currentPath.length - 1];
-  if (currentCave === "end") return [...allPaths, currentPath];
-
-  const pathableCaves = caveMap[currentCave].filter(
-    (destination) =>
-      destination !== "start" &&
-      (hasVisitedSmallCaveTwice(currentPath)
-        ? !(isSmallCave(destination) && currentPath.includes(destination))
-        : true)
-  );
-
-  if (pathableCaves.length < 1) {
-    return allPaths;
-  }
-  return pathableCaves.flatMap((pathable) =>
-    pather2([...currentPath, pathable], caveMap, allPaths)
-  );
-};
-
-export const numPathsCaves2 = (input: string) => {
-  const map = parseCaveMap(parseInput(input));
-  const paths = pather2(["start"], map, []);
-  return paths.length;
-};
+export const numPathsCaves2 = flow(
+  parseInput,
+  parseCaveMap,
+  (caveMap) => pather(["start"], caveMap, [], true),
+  (allPaths) => allPaths.length
+);
